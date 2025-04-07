317da5179b187bca2b54915829c51005df748e1b
acef84 <davikennedy282@gmail.com>
2025-04-07 16:36:10 -0300

    estrutura base de pastas

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BarraPesquisa from './src/Pages/BarraPesquisa';
+import Cadastro from './src/Pages/Cadastro';
+import Home from './src/Pages/Home';
+import Login from './src/Pages/Login';
+import Suporte from './src/Pages/Suporte';
+import ViewJogos from './src/Pages/ViewJogos';
 
 export default function App() {
   return (
