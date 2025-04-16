3049d9a49c00c58c9da1329b2cab0d6ec86b7271
IASMIN VITORIA DOS ANJOS SANTANA <iasmin.vasantana@senacsp.edu.br>
2025-04-16 14:18:37 -0300

    commit da pagina jogos

--- src/Pages/jogos/Nav/index.js
+++ src/Pages/jogos/Nav/index.js
@@ -5,12 +5,12 @@ export default function Nav() {
   return (
     <View style={styles.container}>
       <View style={styles.logo}>
-        <Image style={styles.outroNomeMini} source={require('../../../../assets/safeStar.png')} />
+        <Image style={styles.outroNomeMini} source={require('../../../../../assets/safeStar.png')} />
       </View>
       <View style={styles.icones}>
-        <Image style={styles.outroNomeMini} source={require('../../../../assets/notificacoes.png')} />
-        <Image style={styles.outroNomeMini} source={require('../../../../assets/pesquisar.png')} />
-        <Image style={styles.outroNomeMini} source={require('../../../../assets/minhaConta.png')} />
+        <Image style={styles.outroNomeMini} source={require('../../../../../assets/notificacoes.png')} />
+        <Image style={styles.outroNomeMini} source={require('../../../../../assets/pesquisar.png')} />
+        <Image style={styles.outroNomeMini} source={require('../../../../../assets/minhaConta.png')} />
       </View>
     </View>
   );
