1bba0ca3b4ce0b16768ced549132ac11292d718e
acef84 <davikennedy282@gmail.com>
2025-04-17 15:28:07 -0300

    03 atualização do projeto

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { View } from 'react-native';
-import React from 'react';
-import TelaDePesquisa from './src/Pages/BarraPesquisa';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BarraPesquisa from './src/Pages/BarraPesquisa';
+import Cadastro from './src/Pages/Cadastro';
+import Home from './src/Pages/Home';
+import Login from './src/Pages/Login';
+import Suporte from './src/Pages/Suporte';
+import ViewJogos from './src/Pages/ViewJogos';
 
 export default function App() {
   return (
-    <View>
-      <TelaDePesquisa/>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
