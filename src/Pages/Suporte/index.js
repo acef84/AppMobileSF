c5eb8b401ca8b9e73cef0066dfc52fd7a3db13f9
IASMIN VITORIA DOS ANJOS SANTANA <iasmin.vasantana@senacsp.edu.br>
2025-04-23 15:05:26 -0300

    ajuste na tela de suporte

--- src/Pages/Suporte/index.js
+++ src/Pages/Suporte/index.js
@@ -6,6 +6,7 @@ export * from './FooterS'
 
 import React from 'react';
 import { View } from 'react-native'
+import { StyleSheet } from 'react-native';
 import NavS from './NavS';
 import Header from './Header';
 import Forms from './Forms';
@@ -14,11 +15,20 @@ import Dicas from './Dicas';
 
 export default function TelaSuporte(){
     return(
-        <View>
+        <View style={styles.container}>
             <NavS/>
             <Header/>
             <Forms/>
             <Dicas/>
         </View>
+
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#750000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
